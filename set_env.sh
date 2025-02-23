8f9bdd456ddc1b499a9898495ad7ee7059c82d96
Rishabh Dixit <rdrishabh38@gmail.com>
2025-02-24 01:41:05 +0530

    referencing correct port now

--- set_env.sh
+++ set_env.sh
@@ -5,7 +5,7 @@
 # DO NOT PUSH CREDENTIALS TO GIT. THIS IS JUST A SETUP FOR LOCAL DEVELOPMENT.
 
 export db_host="postgres_master"       # Docker Compose service name for your PostgreSQL container
-export db_port="5433"               # Internal port (not the mapped external port)
+export db_port="5432"               # Internal port (not the mapped external port)
 export db_username="admin"
 export db_password="admin"
 export db_name="master_db"
